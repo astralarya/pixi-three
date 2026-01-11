556477e349a506ae6b73db745daaeab9a3cdf831
Mara Kim <mara@astralarium.net>
2026-01-11 00:59:49 -0800

    juice

--- doc/examples/spinny-cube.tsx
+++ doc/examples/spinny-cube.tsx
@@ -2,41 +2,43 @@ import { useRef, useState } from "react";
 import { type ThreeElements, useFrame } from "@react-three/fiber";
 import { type Mesh } from "three";
 import { TextureNode } from "three/webgpu";
-import { Graphics } from "pixi.js";
+import { Container, Graphics } from "pixi.js";
 import { extend, useTick } from "@pixi/react";
 
 import { PixiTexture } from "#pixi-texture";
-import { usePixiTextureContext } from "#pixi-texture-context";
+import {
+  usePixiTextureContext,
+  usePixiTextureEvents,
+} from "#pixi-texture-context";
 
 extend({ Graphics });
 
 export function SpinnyCube(props: ThreeElements["mesh"]) {
-  // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef<Mesh>(null);
-  // Hold state for hovered and clicked events
-  const [_hovered, hover] = useState(false);
+  const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
+  console.log(clicked);
+
   useFrame((_state, delta) => {
-    ref.current!.rotation.x += delta;
-    ref.current!.rotation.y += delta * 0.5;
+    ref.current!.rotation.x += delta * (hovered ? 0.2 : 1);
+    ref.current!.rotation.y += delta * 0.5 * (hovered ? 0.2 : 1);
   });
 
   const pixiTexture = useRef<TextureNode>(null!);
+  const containerRef = useRef<Container>(null!);
+  const eventHandlers = usePixiTextureEvents(containerRef, {
+    onClick: () => click((x) => !x),
+    onPointerOver: () => hover(true),
+    onPointerOut: () => hover(false),
+  });
 
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={() => click(!clicked)}
-      onPointerOver={() => hover(true)}
-      onPointerOut={() => hover(false)}
-    >
+    <mesh {...props} ref={ref} scale={clicked ? 1.5 : 1} {...eventHandlers}>
       <boxGeometry args={[1, 1, 1]} />
       <meshBasicNodeMaterial>
         <PixiTexture
           ref={pixiTexture}
+          containerRef={containerRef}
           width={clicked ? 64 : 128}
           height={clicked ? 64 : 128}
           attach="colorNode"
@@ -68,24 +70,28 @@ function SpinnyCubeTexture() {
   const star2_color2 = useRef(randomColor());
 
   const { render } = usePixiTextureContext();
-  const time = useRef(0);
+  const time1 = useRef(0);
+  const time2 = useRef(0);
   useTick((ticker) => {
-    time.current += ticker.deltaMS;
-    star1.current.rotation = ((time.current % 4000) / 4000) * 2 * Math.PI;
-    star2.current.scale = Math.sin((time.current / 1000 / 2) * Math.PI) + 1.5;
+    time1.current += ticker.deltaMS * (hover1 ? 0.2 : 1);
+    time2.current += ticker.deltaMS * (hover2 ? 0.2 : 1);
+    star1.current.rotation = ((time1.current % 4000) / 4000) * 2 * Math.PI;
+    star2.current.scale = Math.sin((time2.current / 1000 / 2) * Math.PI) + 1.5;
     render();
   });
-
-  function drawBackground(graphics: Graphics) {
-    graphics.clear();
-    graphics.rect(0, 0, 128, 128).fill(0x000000);
-  }
 
   function drawStar1(graphics: Graphics) {
     graphics.clear();
     graphics.star(64, 64, 5, 32).stroke({
       width: 8,
-      color: hover1 ? star1_color1.current : star1_color2.current,
+      color: star1_color1.current,
+    });
+  }
+  function drawStar1_hover(graphics: Graphics) {
+    graphics.clear();
+    graphics.star(64, 64, 5, 32).stroke({
+      width: 8,
+      color: star1_color2.current,
     });
   }
 
@@ -93,23 +99,32 @@ function SpinnyCubeTexture() {
     graphics.clear();
     graphics.star(64, 64, 5, 32).stroke({
       width: 8,
-      color: hover2 ? star2_color1.current : star2_color2.current,
+      color: star2_color1.current,
+    });
+  }
+
+  function drawStar2_hover(graphics: Graphics) {
+    graphics.clear();
+    graphics.star(64, 64, 5, 32).stroke({
+      width: 8,
+      color: star2_color2.current,
     });
   }
 
   return (
     <>
-      <pixiGraphics draw={drawBackground} />
       <pixiGraphics
         ref={star1}
-        draw={drawStar1}
+        eventMode="static"
+        draw={hover1 ? drawStar1_hover : drawStar1}
         origin={64}
         onPointerEnter={() => setHover1(true)}
         onPointerLeave={() => setHover1(false)}
       />
       <pixiGraphics
         ref={star2}
-        draw={drawStar2}
+        eventMode="static"
+        draw={hover2 ? drawStar2_hover : drawStar2}
         origin={64}
         onPointerEnter={() => setHover2(true)}
         onPointerLeave={() => setHover2(false)}
